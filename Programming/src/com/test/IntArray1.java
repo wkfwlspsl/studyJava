df4f67291b38af010e539c33f99fcf3f476b52de
sj_94128 <sj_94128@naver.com>
2019-11-08 11:40:18 +0900

    java

---
+++ Programming/src/com/test/IntArray1.java
@@ -0,0 +1,40 @@
+package com.test;
+
+/*
+	정수 배열(int array)가 주어지면 이어지는 원소들의 합중 가장 큰값을 구하시오. 단, 시간복잡도는 O(n).
+	Given an integer array, find the largest consecutive sum of elements.
+	예제}
+
+	Input: [-1, 3, -1, 5]
+	Output: 7 // 3 + (-1) + 5
+
+	Input: [-5, -3, -1]
+	Output: -1 // -1
+
+	Input: [2, 4, -2, -3, 8]
+	Output: 9 // 2 + 4 + (-2) + (-3) + 8
+ */
+public class IntArray1 {
+	public static void main(String[] args) {
+		int[] arr1 = {-1, 3, -1, 5};
+		int[] arr2 = {-5, -3, -1};
+		int[] arr3 = {2, 4, -2, -3, 8};
+		
+		System.out.println(solution(arr1));
+		System.out.println(solution(arr2));
+		System.out.println(solution(arr3));
+	}
+	public static int solution(int[] arr) {
+		int currentSum = arr[0];
+		int maxSum = arr[0];
+		
+		for(int i=1; i<arr.length; i++) {
+			currentSum = Math.max(currentSum + arr[i], arr[i]);
+			maxSum = Math.max(currentSum, maxSum);
+		}
+		
+		return maxSum;
+	}
+}
+
+
---
+++ Programming/src/com/test/IntArray2.java
@@ -0,0 +1,24 @@
+package com.test;
+
+/*
+	정수 배열과 정수 k가 주어지면 모든 원소를 k칸씩 앞으로 옮기시오.
+
+	Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+	input: [1, 2, 3, 4, 5], k=2
+	output: [3, 4, 5, 1, 2]
+
+	input: [0, 1, 2, 3, 4], k=1
+	output: [1, 2, 3, 4, 0]
+
+	시간복잡도를 최대한 최적화 하시오.
+ */
+public class IntArray2 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int solution(int[] arr) {
+		return 0;
+	}
+}
