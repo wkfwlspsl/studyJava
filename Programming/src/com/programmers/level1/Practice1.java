c74de655e5df548069b21473ece1657d6227c35a
suji <z80289336@gmail.com>
2019-11-21 13:07:55 +0900

    연습문제

--- Programming/src/com/programmers/level1/Practice1.java
+++ Programming/src/com/programmers/level1/Practice1.java
@@ -36,6 +36,7 @@ public class Practice1 {
 	
 	public static void main(String[] args) throws ParseException {
 		System.out.println(solution(5, 24));
+		System.out.println(solution2(5, 24));
 	}
 	
 	public static String solution(int a, int b) throws ParseException {
@@ -63,5 +64,33 @@ public class Practice1 {
 		return sb.toString();
 	}
 	
-	//date관련 api없이 알고리즘으로도 짜보기
+	//date관련 api없이 ..
+	public static String  solution2(int a, int b) {
+		int[] monthArr = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int day = 0;
+		
+		for(int i=0; i<a-1; i++) {
+			day += monthArr[i];
+		}
+		day += b;
+		
+		switch(day%7) {
+			case 0:
+				return "THU";
+			case 1:
+				return "FRI";
+			case 2:
+				return "SAT";
+			case 3:
+				return "SUN";
+			case 4:
+				return "MON";
+			case 5:
+				return "TUE";
+			case 6:
+				return "WED";
+		}
+		
+		return null;
+	}
 }
