34f4df2bf57a457ef58709f4e0d1fd570c6af4f7
suji <z80289336@gmail.com>
2019-12-26 15:55:47 +0900

    퇴근까지 남은시간 알려줘

---
+++ Programming/src/com/project/offwork/OffWork.java
@@ -0,0 +1,49 @@
+package com.project.offwork;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/*
+ * 출근시간, 하루근무시간 입력
+ * 남은시간 분으로 출력
+ */
+public class OffWork {
+	public static void main(String[] args) throws ParseException {
+		System.out.println(solution("8:40",9));
+		
+	}
+	
+	public static String solution(String startTime, int total) {
+		try {
+			
+			int totalWorkingTime = total * 60;
+			String currentTime = getCurrentTime();
+			
+			SimpleDateFormat f = new SimpleDateFormat("HH:mm", Locale.KOREA);
+			Date startDate = f.parse(startTime);
+			Date currentDate = f.parse(currentTime);
+			int diff = (int) (currentDate.getTime() - startDate.getTime());
+			int elapsedTime = diff / 1000 / 60;
+	
+			int offWorkTime = totalWorkingTime - elapsedTime;
+			
+			if(offWorkTime <= 0) {
+				return "퇴근하세요";
+			}else {
+				return String.valueOf(offWorkTime);
+			}
+		}catch(NumberFormatException | ParseException e) {
+			return "출근시간 잘못 입력";
+		}
+	}
+
+	private static String getCurrentTime() {
+		Calendar calendar = Calendar.getInstance();
+		Date date = calendar.getTime();
+		String currentTime = (new SimpleDateFormat("HH:mm").format(date));
+		return currentTime;
+	}
+}
