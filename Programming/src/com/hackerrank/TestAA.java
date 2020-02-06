b84c6b7ea3e03adba8e731c091b6890c5ffbb6ba
suji <z80289336@gmail.com>
2020-02-06 18:07:19 +0900

    code

---
+++ Programming/src/com/hackerrank/TestAA.java
@@ -0,0 +1,41 @@
+package com.hackerrank;
+
+import java.util.Arrays;
+
+/*
+게임보드 점수
+100 90 90 80 75 60
+앨리스 게임 점수
+50 65 77 90 102
+
+출력
+6 5 4 2 1
+ */
+public class TestAA {
+	public static void main(String[] args) {
+		int[] result = solution(new int[] {100,90,90,80,75,60}, new int[] {50,65,77,90,102});
+		System.out.println(result);
+	}
+	
+	public static int[] solution(int[] scores, int[] alice) {
+		int[] result = new int[alice.length];
+		int[] distinctScore = Arrays.stream(scores).distinct().sorted().toArray();
+		for(int i=0; i<alice.length; i++) {
+			int aliceScore = alice[i];
+			result[i] = getAliceRank(distinctScore, aliceScore);
+			
+		}
+		
+		return result;
+	}
+	
+	public static int getAliceRank(int[] distinctScore, int aliceScore) {
+		for(int i=distinctScore.length-1; i>=0; i--) {
+			if(distinctScore[i] <= aliceScore) {
+				return distinctScore.length - i;
+			}
+		}
+		return distinctScore.length + 1;
+	}
+	
+}
--- Programming/src/com/test/Temp.java
+++ Programming/src/com/test/Temp.java
@@ -1,46 +1,15 @@
 package com.test;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 public class Temp {
-	public static void main(String[] args) throws ScriptException {
-		String ruleFormula = "false && (false && (true || (false || false)) && false) || (true || false) && true";
+	public static void main(String[] args) {
+		int[] arr = new int[] {3,1,2,3};
 		
-		ScriptEngineManager sem = new ScriptEngineManager();
-		ScriptEngine engine = sem.getEngineByName("JavaScript"); // 엔진 가져오는게 오래걸림
-		long beforeTime = System.currentTimeMillis();
-		boolean result = (boolean) engine.eval(ruleFormula); // 실제 evaluate는 크게 오래걸리지는 않음
-		long afterTime = System.currentTimeMillis();
+		int max = Arrays.stream(arr).max().getAsInt();
+		long cnt = Arrays.stream(arr).filter(x -> (x == max)).count();
+		System.out.println(cnt);
 		
-		System.out.println(result);
-		long diff = (afterTime - beforeTime);
-		System.out.println(diff);
-		
-		String foo = "sj:1,sh:9,hj:3";
-		Map<String, Integer> map = Arrays.asList(foo.split(",")).stream().map(s -> s.split(":"))
-				.collect(Collectors.toMap(e -> e[0], e -> Integer.parseInt(e[1])));
-		System.out.println(map.toString());
-		
-		int a=1, b=2;
-		engine.put("a", a);
-		engine.put("b", b);
-		String str = "a>b";
-		long st = System.currentTimeMillis();
-		boolean test = (boolean) engine.eval(str);
-		long en = System.currentTimeMillis();
-		long d = en - st;
-		
-		if(test) {
-			System.out.println("참입니다");
-		}else {
-			System.out.println("거짓입니다");
-		}
-		System.out.println(d);
 	}
+	
 }
